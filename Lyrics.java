d52ddeff2660427032cbedcbba2cb97beebbe743
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-04-21 19:11:25 -0400

    Reorganized and cleaned up code before going further

--- Lyrics.java
+++ Lyrics.java
@@ -9,10 +9,11 @@ import org.jsoup.Connection;
 class Lyrics {
 
   public Lyrics(){
-    null;
+
   }
 
   public boolean contains(String url, String title) {
+    /*
     String lyrics = this.getLyrics(url);
     for (int i = 0; i < lyrics.length() - title.length(); i++) {
       if (title.equals(lyrics.substring(i,title.length()))) {
@@ -20,20 +21,26 @@ class Lyrics {
       }
     }
     return false;
+    */
+    return true;
   }
 
   public String getLyrics(String url) {
-    try {
+    /*try {
       Document doc = Jsoup.connect(url).get();
       Elements
 
     } catch (IOException e) {
       e.printStackTrace();
     }
+    */
+    return "";
   }
 
   public static void main(String args[]) {
+    /*
     Songs stuff = new Songs();
     System.out.println(stuff.getLyrics("https://genius.com/Drake-toosie-slide-lyrics"));
+    */
   }
 }
--- Songs.java
+++ Songs.java
@@ -8,33 +8,78 @@ import org.jsoup.Connection;
 
 class Songs {
 
-  public static void main(String args[]) {
-    try {
-      count = 0;
-      Lyrics comparer = new Lyrics();
-      // Here we create a document object and use JSoup to fetch the website
-      Document doc = Jsoup.connect("https://genius.com/#top-songs").get();
+  Double song_count;
 
-      // Find all elements "a" (some are songs)
+  Double contain_count;
+
+  Double percentage;
+  /*
+  * Empty Constructor
+  */
+  public Songs(){
+    song_count = 0.;
+    contain_count = 0.;
+  }
+
+  /*
+  * This method returns the percentage of songs on the top charts whose
+  * title is contained somewhere in the song
+  */
+  public double getPercent(String url) {
+    try {
+      // Create a new Lyrics Comparer Object
+      Lyrics comparer = new Lyrics();
+      // Create a document and fetch the website
+      Document doc = Jsoup.connect(url).get();
+
+      // Find all elements "a", some of which are Songs
       Elements songs = doc.select("a");
       for (Element song : songs) {
 
-        // Check if the current element is a song
-        String attribute = song.attr("class");
-        if(attribute.equalsIgnoreCase("PageGriddesktop-a6v82w-0 ChartItemdesktop__Row-sc-3bmioe-0 qsIlk")){
-          // Get the title and url to the lyrics for that song
-          String title = song.getElementsByClass("ChartSongdesktop__Title-sc-18658hh-3 fODYHn").text();
-          String lyrics_url = song.attr("href");
-          title = title.toLowerCase();
-          if (comparer.compareLyrcis(lyrcis_url, title)) {
-            count++;
+        //Check if the current element is a song
+        if (this.checkElement(song)) {
+          this.song_count+= 1;
+          // Check if the title is contained in the lyrics
+          if (this.contains(song)) {
+            this.contain_count+=1;
           }
         }
       }
-      return count;
-    // In case of any IO errors, we want the messages written to the console
-    } catch (IOException e) {
+      // Calculate and return the percentage
+      this.percentage = this.contain_count / this.song_count;
+      return this.percentage;
+
+      // In case of any IO errors, we want the messages written to the console
+      } catch (IOException e) {
       e.printStackTrace();
+      }
+      return 0.0;
+  }
+
+  public boolean checkElement(Element thing) {
+    String attribute = thing.attr("class");
+    if (attribute.equals("PageGriddesktop-a6v82w-0 ChartItemdesktop__Row-sc-3bmioe-0 qsIlk")) {
+      return true;
     }
+    return false;
+  }
+
+  public boolean contains(Element song) {
+    // Create a new Lyrics Comparer Object
+    Lyrics comparer = new Lyrics();
+    // Get the title and url to the lyrics for that song
+    String title = song.getElementsByClass("ChartSongdesktop__Title-sc-18658hh-3 fODYHn").text();
+    String lyrics_url = song.attr("href");
+    title = title.toLowerCase();
+    // If the title is contained in the lyrics, return true;
+    if (comparer.contains(lyrics_url, title)) {
+      return true;
+    }
+    return false;
+  }
+
+  public static void main(String args[]) {
+    Songs test = new Songs();
+    System.out.println(test.getPercent("https://genius.com/#top-songs"));
   }
 }
